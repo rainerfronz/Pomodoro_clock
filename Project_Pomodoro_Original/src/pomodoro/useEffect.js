3f1f7cc6596804831f45e479c75d58a53a88b35f
Rainer Fronz <rainerfronz@Rainers-MacBook-Air.local>
2021-06-07 23:53:35 -0500

    finally done

--- Project_Pomodoro_Original/src/pomodoro/useEffect.js
+++
@@ -1,9 +0,0 @@
-useInterval(() => {
-    if (session.timeRemaining === 0) {
-      new Audio("https://bigsoundbank.com/UPLOAD/mp3/1482.mp3").play();
-      return setSession(nextSession(focusDuration, breakDuration));
-    }
-    return setSession(nextTick);
-  },
-  isTimerRunning ? 1000 : null
-);
